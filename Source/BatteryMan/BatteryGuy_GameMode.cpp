f6e60db97d2984afdb05c8a9e5279856c9c127e2
Shivam Nagpal <shivamnagpal432@gmail.com>
2019-12-28 13:20:06 +0530

    First Stable Version

--- Source/BatteryMan/BatteryGuy_GameMode.cpp
+++ Source/BatteryMan/BatteryGuy_GameMode.cpp
@@ -2,4 +2,36 @@
 
 
 #include "BatteryGuy_GameMode.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
+
+ABatteryGuy_GameMode::ABatteryGuy_GameMode()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABatteryGuy_GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	FTimerHandle UnusedHandle;
+	GetWorldTimerManager().SetTimer(UnusedHandle, this, &ABatteryGuy_GameMode::SpawnPlayerRecharge, FMath::RandRange(2,5), true);
+}
+
+void ABatteryGuy_GameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABatteryGuy_GameMode::SpawnPlayerRecharge()
+{
+	float RandX = FMath::RandRange(Spawn_X_Min, Spawn_X_Max);
+	float RandY = FMath::RandRange(Spawn_Y_Min, Spawn_Y_Max);
+
+	FVector SpawnPosition = FVector(RandX, RandY, Spawn_Z);
+	FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+
+	GetWorld()->SpawnActor(PlayerRecharge, &SpawnPosition, &SpawnRotation);
+}
+
 
--- Source/BatteryMan/BatteryGuy_GameMode.h
+++ Source/BatteryMan/BatteryGuy_GameMode.h
@@ -7,11 +7,36 @@
 #include "BatteryGuy_GameMode.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class BATTERYMAN_API ABatteryGuy_GameMode : public AGameMode
 {
 	GENERATED_BODY()
-	
+
+public:
+	ABatteryGuy_GameMode();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<APawn> PlayerRecharge;
+
+	float Spawn_Z = 500.f;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_X_Min;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_X_Max;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_Y_Min;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_Y_Max;
+
+	void SpawnPlayerRecharge();
 };
