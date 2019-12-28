d445c954387354196a13e7be0c3f9d36d2d340a1
Shivam Nagpal <shivamnagpal432@gmail.com>
2019-12-28 12:34:23 +0530

    Half Way there

---
+++ Source/BatteryMan/BatteryGuy_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BatteryGuy_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTERYMAN_API ABatteryGuy_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
